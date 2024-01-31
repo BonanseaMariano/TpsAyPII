58361f218b57db289c099e8add5c9ebe88596a5e
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2024-01-31 20:20:00 -0300

    Validacion de solo ingreso de numeros

--- Tp1/Ej1/src/main/java/com/mycompany/ej1/Ej1.java
+++ Tp1/Ej1/src/main/java/com/mycompany/ej1/Ej1.java
@@ -1,6 +1,9 @@
 package com.mycompany.ej1;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 /**
  *
@@ -13,10 +16,42 @@ public class Ej1 {
         
         String[] opciones = {"Suma", "Producto", "Diferencia","Cociente","Volver a ingresar numeros","Salir"};
         
+        JTextField textField = new JTextField();
+        textField.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent evt) {
+                int t;
+                t=evt.getKeyCode();
+                if ((t>=evt.VK_A && t<=evt.VK_Z) || t==evt.VK_SPACE) {
+                    textField.setEditable(false);
+                }else{
+                    textField.setEditable(true);
+                }
+            }
+        });
+         
         int num1,num2,seleccion;
         do {            
-            num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer numero"));
-            num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo numero")); 
+            String[] boton = {"OK","Cancelar"};
+            int temp = JOptionPane.showOptionDialog(null, textField, "Ingrese el primer numero:", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE, null, boton, boton[0]);
+            //Para que se salga si le pone cancelar
+            if (temp == 1) {
+                System.exit(0);
+            }
+            num1 = Integer.parseInt(textField.getText());
+            
+            //Para limpiar el textField
+            textField.setText("");
+            temp = JOptionPane.showOptionDialog(null, textField, "Ingrese el segundo numero:", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE, null, boton, boton[0]);
+            
+            //Para que se salga si le pone cancelar
+            if (temp == 1) {
+                System.exit(0);
+            }
+            num2 = Integer.parseInt(textField.getText()); 
+            
+            //Para limpiar el textField
+            textField.setText("");
+            
             do {                
                 seleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción:", "Menú de opciones", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
                 calcular(num1, num2, seleccion);
